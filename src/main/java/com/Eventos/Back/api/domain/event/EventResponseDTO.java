9cc3809b731efc85198e0291a3fb283d50c7d594
igor bruno <igordf2502@gmail.com>
2026-01-10 01:48:52 -0300

    refactor: mapear Event para EventResponseDTO no service

---
+++ src/main/java/com/Eventos/Back/api/domain/event/EventResponseDTO.java
@@ -0,0 +1,7 @@
+package com.Eventos.Back.api.domain.event;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record EventResponseDTO(UUID id, String title, String description, Date date, String city, String state, Boolean remote, String eventUrl, String imgUrl) {
+}
--- src/main/java/com/Eventos/Back/api/service/EventService.java
+++ src/main/java/com/Eventos/Back/api/service/EventService.java
@@ -2,11 +2,15 @@ package com.Eventos.Back.api.service;
 
 import com.Eventos.Back.api.domain.event.Event;
 import com.Eventos.Back.api.domain.event.EventRequestDTO;
+import com.Eventos.Back.api.domain.event.EventResponseDTO;
 import com.Eventos.Back.api.repositories.EventRepository;
 import com.amazonaws.services.s3.AmazonS3;
-import com.sun.jdi.request.EventRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,53 +20,65 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 @Service
 public class EventService {
 
-    @Value("${aws.bucket.Name}")
+    @Value("${aws.bucket.name}")
     private String bucketName;
 
     @Autowired
     private AmazonS3 s3Client;
 
     @Autowired
-    private EventRepository eventRepository;
+    private EventRepository repository;
 
-    public Event createEvent(EventRequestDTO data) {
+    public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
-        if (data.image() != null) {
-            this.uploadImg(data.image());
+
+        if(data.image() != null){
+            imgUrl = this.uploadImg(data.image());
         }
 
         Event newEvent = new Event();
         newEvent.setTitle(data.title());
         newEvent.setDescription(data.description());
         newEvent.setEventUrl(data.eventUrl());
-        newEvent.setDate((new Date(data.date())));
+        newEvent.setDate(new Date(data.date()));
         newEvent.setImgUrl(imgUrl);
         newEvent.setRemote(data.remote());
 
-        eventRepository.save(newEvent);
+        repository.save(newEvent);
 
         return newEvent;
     }
-    private String uploadImg(MultipartFile multipartFile) {
+
+    public List<EventResponseDTO> getUpcomingEvents(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.repository.findUpcomingEvents(new Date(), pageable);
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(), event.getDescription(), event.getDate(), "", "", event.isRemote(), event.getEventUrl(), event.getImgUrl()))
+                .stream().toList();
+    }
+
+    private String uploadImg(MultipartFile multipartFile){
         String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
-        try {
-            File file = this.convertMultiPartToFile(multipartFile);
-            s3Client.putObject(bucketName, filename,file);
+
+        try{
+            File file = this.convertMultipartToFile(multipartFile);
+            s3Client.putObject(bucketName, filename, file);
             file.delete();
             return s3Client.getUrl(bucketName, filename).toString();
-        } catch (Exception e) {
-            System.out.println("Error ao subir arquivo ");
-            return "null";
+        } catch (Exception e){
+            System.out.println("erro ao subir arquivo");
+            System.out.println(e.getMessage());
+            return "";
         }
     }
 
-    private File convertMultiPartToFile(MultipartFile multipartFile) throws IOException {
+    private File convertMultipartToFile(MultipartFile multipartFile) throws IOException {
         File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
         FileOutputStream fos = new FileOutputStream(convFile);
         fos.write(multipartFile.getBytes());
