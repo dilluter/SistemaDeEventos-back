8be68a798f190940196adc4dfe9942b0ccaab774
igor bruno <igordf2502@gmail.com>
2026-01-10 00:47:03 -0300

    feat(service): implementar regras de negócio para eventos

---
+++ src/main/java/com/Eventos/Back/api/service/EventService.java
@@ -0,0 +1,72 @@
+package com.Eventos.Back.api.service;
+
+import com.Eventos.Back.api.domain.event.Event;
+import com.Eventos.Back.api.domain.event.EventRequestDTO;
+import com.Eventos.Back.api.repositories.EventRepository;
+import com.amazonaws.services.s3.AmazonS3;
+import com.sun.jdi.request.EventRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class EventService {
+
+    @Value("${aws.bucket.Name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Event createEvent(EventRequestDTO data) {
+        String imgUrl = null;
+        if (data.image() != null) {
+            this.uploadImg(data.image());
+        }
+
+        Event newEvent = new Event();
+        newEvent.setTitle(data.title());
+        newEvent.setDescription(data.description());
+        newEvent.setEventUrl(data.eventUrl());
+        newEvent.setDate((new Date(data.date())));
+        newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
+
+        eventRepository.save(newEvent);
+
+        return newEvent;
+    }
+    private String uploadImg(MultipartFile multipartFile) {
+        String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+        try {
+            File file = this.convertMultiPartToFile(multipartFile);
+            s3Client.putObject(bucketName, filename,file);
+            file.delete();
+            return s3Client.getUrl(bucketName, filename).toString();
+        } catch (Exception e) {
+            System.out.println("Error ao subir arquivo ");
+            return "null";
+        }
+    }
+
+    private File convertMultiPartToFile(MultipartFile multipartFile) throws IOException {
+        File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return convFile;
+    }
+}
